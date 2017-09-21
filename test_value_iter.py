1dfa9d3a4416a1b851338a99de8bfa98edc782ef
Steven H. Wang <wang.steven.h@gmail.com>
2017-09-20 21:30:32 -0700

    End value iteration upon update threshold
    This commit breaks the unit tests because softmax value iteration
    isn't converging.

--- test_value_iter.py
+++ test_value_iter.py
@@ -3,51 +3,57 @@ import numpy as np
 from numpy import testing as t
 
 from mdp import GridWorldMDP
-from value_iter import backwards_value_iter
+from value_iter import backwards_value_iter, _calc_max_update
 
 ni = float('-inf')
+
+class TestUtilities(TestCase):
+    def test_calc_max_update(self):
+        t.assert_allclose(0, _calc_max_update(np.array([ni, ni]), np.array([ni, ni])))
+        t.assert_allclose(1, _calc_max_update(np.array([0, ni]), np.array([-1, ni])))
+        t.assert_allclose(float('inf'), _calc_max_update(np.array([0, 5]), np.array([-1, ni])))
+        t.assert_allclose(2,
+                _calc_max_update(np.array([1, 0, ni, 7]), np.array([1.1, 2, ni, 6.5])))
 
 class TestBackwardsValueIter(TestCase):
     def test_initial(self):
         g = GridWorldMDP(3, 1, {(0, 0): -1, (0, 1): 1, (1, 1): 2, (1, 0): 3})
-        t.assert_allclose(backwards_value_iter(g, 0, iters=0), [0, ni, ni])
-        t.assert_allclose(backwards_value_iter(g, 1, iters=0), [ni, 0, ni])
-        t.assert_allclose(backwards_value_iter(g, 2, iters=0), [ni, ni, 0])
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=0), [0, ni, ni])
+        t.assert_allclose(backwards_value_iter(g, 1, max_iters=0), [ni, 0, ni])
+        t.assert_allclose(backwards_value_iter(g, 2, max_iters=0), [ni, ni, 0])
 
     def test_easy_no_rewards(self):
         g = GridWorldMDP(3, 1, {})
-        t.assert_allclose(backwards_value_iter(g, 0, iters=0), [0, ni, ni])
-        t.assert_allclose(backwards_value_iter(g, 1, iters=0), [ni, 0, ni])
-        t.assert_allclose(backwards_value_iter(g, 2, iters=0), [ni, ni, 0])
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=0), [0, ni, ni])
+        t.assert_allclose(backwards_value_iter(g, 1, max_iters=0), [ni, 0, ni])
+        t.assert_allclose(backwards_value_iter(g, 2, max_iters=0), [ni, ni, 0])
 
-        t.assert_allclose(backwards_value_iter(g, 0, iters=1), [0, 0, ni])
-        t.assert_allclose(backwards_value_iter(g, 1, iters=1), [0, 0, 0])
-        t.assert_allclose(backwards_value_iter(g, 2, iters=1), [ni, 0, 0])
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=1), [0, 0, ni])
+        t.assert_allclose(backwards_value_iter(g, 1, max_iters=1), [0, 0, 0])
+        t.assert_allclose(backwards_value_iter(g, 2, max_iters=1), [ni, 0, 0])
 
-        t.assert_allclose(backwards_value_iter(g, 0, iters=2),
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=2),
                 [np.log(2), np.log(2), 0])
-        t.assert_allclose(backwards_value_iter(g, 0, iters=3),
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=3),
                 [np.log(4), np.log(5), np.log(3)])
-        t.assert_allclose(backwards_value_iter(g, 0), [np.log(4), np.log(5), np.log(3)])
 
     def test_easy_neg_one_reward(self):
         g = GridWorldMDP(3, 1, {}, default_reward=-1)
-        t.assert_allclose(backwards_value_iter(g, 0, iters=0), [0, ni, ni])
-        t.assert_allclose(backwards_value_iter(g, 0, iters=1), [0, -1, ni])
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=0), [0, ni, ni])
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=1), [0, -1, ni])
 
         V_2 = [np.log(1 + np.exp(-2)), np.log(2) - 1, -2]
-        t.assert_allclose(backwards_value_iter(g, 0, iters=2), V_2)
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=2), V_2)
 
         V_3 = [np.log(np.exp(np.log(1 + np.exp(-2))) + np.exp(np.log(2) - 2)),
                 np.log(np.exp(np.log(1 + np.exp(-2)) - 1) + np.exp(np.log(2) - 1)
                     + np.exp(-3)),
                 np.log(np.exp(np.log(2) - 2) + np.exp(-2))]
-        t.assert_allclose(backwards_value_iter(g, 0, iters=3), V_3)
-        t.assert_allclose(backwards_value_iter(g, 0), V_3)
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=3), V_3)
 
     def test_easy_neg_one_reward_plus_special(self):
         g = GridWorldMDP(3, 1, {(2,0): -10}, default_reward=-1)
-        t.assert_allclose(backwards_value_iter(g, 0, iters=0), [0, ni, ni])
-        t.assert_allclose(backwards_value_iter(g, 0, iters=1), [0, -1, ni])
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=0), [0, ni, ni])
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=1), [0, -1, ni])
         V_2 = [np.log(1 + np.exp(-2)), np.log(2) - 1, -11]
-        t.assert_allclose(backwards_value_iter(g, 0, iters=2), V_2)
+        t.assert_allclose(backwards_value_iter(g, 0, max_iters=2), V_2)
--- value_iter.py
+++ value_iter.py
@@ -1,9 +1,32 @@
 import numpy as np
 import warnings
 
-def backwards_value_iter(mdp, init_state, iters=None):
+def _calc_max_update(V, V_prime):
     """
-    Calculate the value of reaching all states, starting from a given initial state.
+    Use this rather than max(np.absolute(V_prime - V)) because an unchanged value
+    of float('-inf') would result in a NaN change, when it should actually result
+    in a 0 change.
+    """
+
+    assert V.shape == V_prime.shape, (V, V_prime)
+    assert float('nan') not in V, V
+    assert float('nan') not in V_prime, V_prime
+
+    max_update = float('-inf')
+    for v, v_p in zip(V, V_prime):
+        if v == v_p == float('-inf'):
+            # v - v_p would return NaN, but in our simulation, this is actually
+            # a zero update.
+            max_update = max(max_update, 0)
+        else:
+            max_update = max(max_update, abs(v - v_p))
+
+    return max_update
+
+def backwards_value_iter(mdp, init_state, states=None, update_threshold=1e-7, max_iters=None):
+    """
+    Approximate the softmax value of reaching various destination states, starting
+    from a given initial state.
 
     Params:
         S [int]: The number of states.
@@ -14,8 +37,11 @@ def backwards_value_iter(mdp, init_state, iters=None):
             received from taking action a at state s.
         transition [function]: The state transition function for the deterministic MDP.
             transition(s, a) returns the state that results from taking action a at state s.
-        iters [int]: (optional) The number of value iterations to perform. By default,
-            iters will be set to S.
+        update_threshold [float]: (optional) When the magnitude of all value updates is
+            less than update_threshold, value iteration will return its approximate solution.
+        max_iters [int]: (optional) An upper bound on the number of value iterations to
+            perform. If this upper bound is reached, then iteration will cease regardless
+            of whether `update_threshold`'s condition is met.
 
     Returns:
         value [np.ndarray]: A length S array, where the ith element is the value of
@@ -24,11 +50,12 @@ def backwards_value_iter(mdp, init_state, iters=None):
     assert init_state >= 0 and init_state < mdp.S, init_state
     V = np.array([float('-inf')] * mdp.S)
     V[init_state] = 0
-    if iters == None:
-        iters = mdp.S
+    if max_iters == None:
+        max_iters = float('inf')
 
-    rewards_by_state = np.zeros(mdp.S)
-    for i in range(iters):
+    max_update = float('inf')
+    it = 0
+    while max_update > update_threshold and it < max_iters:
         V_prime = np.zeros(mdp.S)
         for s_prime in range(mdp.S):
             for a in range(mdp.A):
@@ -36,7 +63,11 @@ def backwards_value_iter(mdp, init_state, iters=None):
                 V_prime[s] += np.exp(mdp.rewards[s_prime, a] + V[s_prime])
 
         warnings.filterwarnings("ignore", "divide by zero encountered in log")
-        V = np.log(V_prime)
+        V_prime = np.log(V_prime)
         warnings.resetwarnings()
+
+        max_update = _calc_max_update(V, V_prime)
+        it += 1
+        V = V_prime
 
     return V
