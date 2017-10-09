63a88fa20ab3d74bfc8a71800e11a0867a57bb09
Steven H. Wang <wang.steven.h@gmail.com>
2017-10-09 10:23:53 -0700

    Add beta modification to simulate trajectory

--- inference.py
+++ inference.py
@@ -33,7 +33,7 @@ def _display(mdp, traj, init_state, goal_state, overlay=False):
                 line[goal_state[1]] = 'G' if goal_state in visited else 'g'
         print(line)
 
-def simulate(mdp, initial_state, goal_state, path_length=None):
+def simulate(mdp, initial_state, goal_state, beta=1, path_length=None):
     """
     Generate a sample trajectory of a softmax agent's behavior.
 
@@ -41,14 +41,20 @@ def simulate(mdp, initial_state, goal_state, path_length=None):
         mdp [GridWorldMDP]: The world that the agent is acting in.
         initial_state [int]: The state that the agent starts in.
         dest_state [int]: The agent's goal state.
+        beta [float]: (optional) The softmax agent's irrationality constant.
+            Beta must be nonnegative. If beta=0, then the softmax agent
+            always acts optimally. If beta=float('inf'), then the softmax agent
+            acts completely randomly.
         path_length [int]: (optional) If the returned trajectory
             has length more than `path_length`, the return
             value is truncated.
 
-    Return:
+    Returns:
         traj [list]: A list of (int, int) pairs, representing
             the agent's state and action at each timestep.
     """
+    assert beta >= 0, beta
+
     mdp = mdp.copy()
     mdp.set_goal(goal_state)
 
@@ -61,7 +67,7 @@ def simulate(mdp, initial_state, goal_state, path_length=None):
     traj = []
     s = initial_state
     while len(traj) < path_length:
-        a = sample_action(mdp, s, goal_state, V)
+        a = sample_action(mdp, s, goal_state, beta=beta, cached_values=V)
         traj.append([s, a])
         if a == mdp.Actions.ABSORB:
             break
@@ -69,21 +75,34 @@ def simulate(mdp, initial_state, goal_state, path_length=None):
             s = mdp.transition(s, a)
     return traj
 
-def sample_action(mdp, state, goal, cached_values=None):
+def sample_action(mdp, state, goal, beta=1, cached_values=None):
     """
     Choose an action probabilistically, like a softmax agent would.
     Params:
         mdp [GridWorldMDP]: The MDP that the agent is playing.
         state [int]: The state that the agent is in.
+        goal [int]: The agent's goal state.
+        beta [float]: (optional) The softmax agent's irrationality constant.
+            Beta must be nonnegative. If beta=0, then the softmax agent
+            always acts optimally. If beta=float('inf'), then the softmax agent
+            acts completely randomly.
         cached_values [np.ndarray]: (optional) Precalculated values from all states to goal.
             (Calculated using forward value iteration)
     Return:
         a [int]: An action.
     """
-    mdp.set_goal(goal)
+    # TODO(sirspinach): Unit tests, especially for beta edge cases. (low priority)
+    assert beta >= 0, beta
+
+    # TODO(sirspinach): This uniform choice also chooses ABSORB.
+    #                   Get rid of this possibility for non-goal states.
+    if beta == np.inf:
+        # Uniform choice, would result in P = 0/0 = NaN.
+        return np.random.choice(list(range(mdp.A)))
 
     if cached_values is not None:
         V = cached_values
+        mdp.set_goal(goal)
     else:
         V = forwards_value_iter(mdp, goal, max_iters=1000)
 
@@ -92,7 +111,15 @@ def sample_action(mdp, state, goal, cached_values=None):
         s_prime = mdp.transition(state, a)
         P[a] = mdp.rewards[state, a] + V[s_prime] - V[state]
 
-    P = np.exp(P)
+    if beta == 0:
+        # Hardmax choice, would result in P = inf/inf = NaN.
+        argmax = np.array(np.argmax(P))
+        if argmax.shape == tuple():
+            return argmax
+        else:
+            return np.random.choice(argmax)
+
+    P = np.exp(P/beta)
     P = P / sum(P)
     return np.random.choice(list(range(mdp.A)), p=P)
 
