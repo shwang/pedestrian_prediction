8ea5cb5fdde969445090a035ab89c4d45262fe49
Steven H. Wang <wang.steven.h@gmail.com>
2017-12-10 17:11:19 -0800

    update plotting utilities

--- pp/plot/__main__.py
+++ pp/plot/__main__.py
@@ -1,7 +1,10 @@
 import common
 import common_multi
+import common_forget
 
 if __name__ == '__main__':
-    common.simple_traj_inf("diag", R=-1, traj_len=7, save_png=False)
+    common_forget.traj_inf("diag-fickle2", R=-1,
+            N=20,
+            traj_lens=[6,8,None], save_png=True)
     # common_multi.multidest_traj_inf(
     #         "diag", "tri", N=40, R=-1, save_png=True)
--- pp/plot/common.py
+++ pp/plot/common.py
@@ -3,7 +3,7 @@ import numpy as np
 from ..mdp import GridWorldMDP
 from ..inference.softmax.destination import infer_destination
 
-from ..util import sum_rewards, display, build_traj_from_actions
+from ..util import build_traj_from_actions
 from ..util.hardmax import simulate, sample_action
 
 from ..parameters import inf_default
@@ -11,7 +11,7 @@ from ..parameters import inf_default
 Actions = GridWorldMDP.Actions
 
 
-def make_heat_map(g, occupancies, traj_or_start_state, stars,
+def make_heat_map(g, occupancies, traj, stars,
         zmin=None, zmax=0, auto_logarithm=True):
     import plotly.graph_objs as go
     data = []
@@ -23,15 +23,13 @@ def make_heat_map(g, occupancies, traj_or_start_state, stars,
     hm = go.Heatmap(z=o, zmin=zmin, zmax=zmax, name="log expected occupancy")
     data.append(hm)
 
-    try:
-        states = [s for s, a in traj_or_start_state]
-        states.append(g.transition(*traj_or_start_state[-1]))
-    except TypeError:
-        states = [traj_or_start_state]
+    states = [s for s, a in traj]
+    states.append(g.transition(*traj[-1]))
+
     coors = [g.state_to_coor(s) for s in states]
     x, y = zip(*coors)
-    traj_line = dict(x=x, y=y, line=dict(color='white', width=3))
-    data.append(traj_line)
+    line = dict(x=x, y=y, line=dict(color='white', width=3))
+    data.append(line)
 
     if len(stars) > 0:
         x, y = zip(*[g.state_to_coor(s) for s in stars])
@@ -42,28 +40,42 @@ def make_heat_map(g, occupancies, traj_or_start_state, stars,
     return data
 
 
-def plot_heat_maps(g, traj_or_start_state, occupancy_list, title_list,
+def plot_heat_maps(g, traj_or_trajs, occupancy_list, title_list,
         stars_grid=None, zmin=None, zmax=None, auto_logarithm=True, **kwargs):
     """
+    traj_or_trajs: A trajectory (list of state-action pairs), or a list of
+        trajectories.
     stars_grid: A list of lists. The ith list is a list of states on which to place
         stars in the ith heat map.
             OR a single list, which is used for every heat map.
     """
     subplot_list = []
 
+    L = len(occupancy_list)
+
     if stars_grid == None:
         stars_grid = []
+    else:
+        stars_grid = list(stars_grid)
 
-    stars_grid = list(stars_grid)
     try:
         iter(stars_grid[0])
     except TypeError:
-        stars_grid = [stars_grid] * len(occupancy_list)
-    assert len(stars_grid) == len(occupancy_list)
+        stars_grid = [stars_grid] * L
+    assert len(stars_grid) == L
 
-    for o, stars in zip(occupancy_list, stars_grid):
+    try:
+        # If iterable at two-deep, then this is a list of traj.
+        iter(traj_or_trajs[0])
+        trajs = traj_or_trajs
+    except TypeError:
+        # Assume this is a single trajectory.
+        trajs = [traj_or_trajs] * L
+    assert len(trajs) == L, trajs
+
+    for o, stars, traj in zip(occupancy_list, stars_grid, trajs):
         o = o.reshape(g.rows, g.cols)
-        trace = make_heat_map(g, o, traj_or_start_state, stars,
+        trace = make_heat_map(g, o, traj, stars,
                 zmin=zmin, zmax=zmax, auto_logarithm=auto_logarithm)
         subplot_list.append(trace)
 
@@ -106,12 +118,13 @@ def _occ_starter(N, R, mode):
     modes: diag, diag-top, vertical, diag-but-top
     """
     g = GridWorldMDP(N, N, {}, default_reward=R)
+    one = g.coor_to_state(1,1)
     T = N+N
     if mode == "diag":
-        start = 0
+        start = one
         goal = model_goal = g.S - 1
     elif mode == "diag-top":
-        start = 0
+        start = one
         goal = model_goal = g.coor_to_state(N//2, N-1)
     elif mode == "vertical":
         start = g.coor_to_state(N//2, 0)
@@ -126,32 +139,33 @@ def _occ_starter(N, R, mode):
 def _traj_starter(N, init_state, mode):
     A = Actions
     g = GridWorldMDP(N, N)
+    one = g.coor_to_state(1,1)
     if mode == "diag":
-        start = 0
-        actions = [A.UP_RIGHT] * (N-1)
+        start = one
+        actions = [A.UP_RIGHT] * (N-2)
     elif mode == "horizontal":
         start = g.coor_to_state(0, N//2)
-        actions = [A.RIGHT] * (N-1)
+        actions = [A.RIGHT] * (N-2)
     elif mode == "horizontal_origin":
-        start = 0
-        actions = [A.RIGHT] * (N-1)
+        start = one
+        actions = [A.RIGHT] * (N-2)
     elif mode == "vertical":
         start = g.coor_to_state(N//2, 0)
-        actions = [A.UP] * (N-1)
+        actions = [A.UP] * (N-2)
     elif mode == "diag-crawl":
-        start = 0
-        actions = [A.RIGHT] * (N-1) + [A.UP] * (N-1)
+        start = one
+        actions = [A.RIGHT] * (N-2) + [A.UP] * (N-2)
     elif mode == "diag-fickle":
-        start = 0
+        start = one
         w = (N-1)//2
         W = N - 1 - w
-        actions = [A.RIGHT] * w + [A.UP_RIGHT] * W + \
+        actions = [A.RIGHT] * w + [A.UP_RIGHT] * (W-1) + \
                 [A.UP] * w
     elif mode == "diag-fickle2":
-        start = 0
+        start = one
         w = (N-1)//2
         W = N - 1 - w
-        actions = [A.UP_RIGHT] * W + [A.DOWN_RIGHT] * w \
+        actions = [A.UP_RIGHT] * (W-1) + [A.DOWN_RIGHT] * w \
                 + [A.DOWN]
     else:
         raise Exception("invalid mode: {}".format(mode))
@@ -159,8 +173,7 @@ def _traj_starter(N, init_state, mode):
 
 
 def _traj_beta_inf_loop(on_loop, g, traj, goal, inf_mod=inf_default, guess=1,
-        traj_len=None, min_beta=0.01, max_beta=100, verbose=True):
-    traj_len = traj_len or np.inf
+        min_beta=0.01, max_beta=100, verbose=True):
     for i in xrange(len(traj) + 1):
         if i == 0:
             start = traj[0][0]
@@ -168,8 +181,6 @@ def _traj_beta_inf_loop(on_loop, g, traj, goal, inf_mod=inf_default, guess=1,
             beta_hat = guess
         else:
             tr = traj[:i]
-            if len(tr) > traj_len:
-                tr = tr[-traj_len:]
             beta_hat = inf_mod.beta.binary_search(g, tr, goal, guess=beta_hat,
                     verbose=verbose, min_beta=min_beta, max_beta=max_beta)
             if verbose:
@@ -177,8 +188,8 @@ def _traj_beta_inf_loop(on_loop, g, traj, goal, inf_mod=inf_default, guess=1,
         on_loop(tr, round(beta_hat, 3), i)
 
 
-def simple_ground_truth_inf(mode="diag", N=30, R=-6, true_beta=5,
-        zmin=-5, zmax=0, inf_mod=inf_default, title=None, traj_len=None,
+def simple_ground_truth_inf(mode="diag", N=30, R=-1, true_beta=5,
+        zmin=-5, zmax=0, inf_mod=inf_default, title=None,
         **kwargs):
     g, T, start, goal, model_goal = _occ_starter(N, R, mode)
 
@@ -210,11 +221,11 @@ def simple_ground_truth_inf(mode="diag", N=30, R=-6, true_beta=5,
         plot_heat_maps(g, traj, occ_list, subplot_titles, title=title,
                 stars_grid=stars_grid, zmin=zmin, zmax=zmax, **kwargs)
 
-    _traj_beta_inf_loop(on_loop, g, traj, goal, traj_len=traj_len)
+    _traj_beta_inf_loop(on_loop, g, traj, goal)
 
 
-def simple_traj_inf(traj_or_traj_mode="diag", mode="diag", N=30, R=-6, title=None,
-        inf_mod=inf_default, zmin=-5, zmax=0, traj_len=None, **kwargs):
+def simple_traj_inf(traj_or_traj_mode="diag", mode="diag", N=30, R=-1, title=None,
+        inf_mod=inf_default, zmin=-5, zmax=0, **kwargs):
     # We don't care about model_goal. The star we show is always `goal`.
     g, T, start, goal, _ = _occ_starter(N, R, mode)
     if type(traj_or_traj_mode) is str:
@@ -245,4 +256,4 @@ def simple_traj_inf(traj_or_traj_mode="diag", mode="diag", N=30, R=-6, title=Non
         plot_heat_maps(g, traj, occ_list, subplot_titles, title=_title,
                 stars_grid=stars_grid, zmin=zmin, zmax=zmax, **kwargs)
 
-    _traj_beta_inf_loop(on_loop, g, traj, goal, traj_len=traj_len)
+    _traj_beta_inf_loop(on_loop, g, traj, goal)
