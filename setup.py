dce2bd58c37dd57af59535ea0b2106f8a44a793b
abajcsy <andrea.victoria.b@gmail.com>
2018-08-29 13:15:44 -0700

    fixed setup script

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='pedestrian_prediction', version='0.1.0', packages=['pedestrian_prediction'])
+setup(
+    name='pedestrian_prediction',
+    version='0.0.1',
+    packages=find_packages()
+    )
+
+#setup(name='pedestrian_prediction', version='0.1.0', packages=['pedestrian_prediction'])
+
