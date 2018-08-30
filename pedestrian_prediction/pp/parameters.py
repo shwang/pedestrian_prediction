19ed9e274d89d217e2137f67aaf04f4aa63d0897
Steven H. Wang <wang.steven.h@gmail.com>
2018-08-30 14:40:29 -0700

    Simplify parameters.py

--- pedestrian_prediction/pp/parameters.py
+++ pedestrian_prediction/pp/parameters.py
@@ -1,26 +1,20 @@
 import sys
 
-# XXX: Figure this crazy import mess another day.
-# I'm guessing this is because hardmax.occupancy
-# which is part of inference.hardmax, is importing
-# this module.
+def load(module, prefix='pedestrian_prediction.'):
+    """
+    Attempt to return the module and then trying with the prefix.
+    Prefix is used for compatibility with crazyflie_human repo.
+
+    KeyError if module is not found.
+    """
+    return sys.modules.get(module, None) or sys.modules[prefix+module]
+
 import mdp.euclid
 import mdp.hardmax
-
-if 'pp.mdp.euclid' in sys.modules:
-    val_euclid = sys.modules['pp.mdp.euclid']
-else:
-    val_euclid = sys.modules['pedestrian_prediction.pp.mdp.euclid']
-
-if 'pp.mdp.hardmax' in sys.modules:
-    val_hardmax = sys.modules['pp.mdp.hardmax']
-else:
-    val_hardmax = sys.modules['pedestrian_prediction.pp.mdp.hardmax']
-
+val_euclid = load('pp.mdp.euclid')
+val_hardmax = load('pp.mdp.hardmax')
 val_default = val_hardmax
 
 import inference.hardmax
-if 'pp.inference.hardmax' in sys.modules:
-    inf_default = sys.modules['pp.inference.hardmax']
-else:
-    inf_default = sys.modules['pedestrian_prediction.pp.inference.hardmax']
+inf_hardmax = load('pp.inference.hardmax')
+inf_default = load('pp.inference.hardmax')
