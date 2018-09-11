2ea2426da8dd9526b5854750832f0343d747badc
Steven H. Wang <wang.steven.h@gmail.com>
2018-09-11 16:01:10 -0700

    Restructure module, install dependcies
    setup.py now installs dependencies automatically. Also, this commit
    removes an extraneous pedestrian_prediction top-level module.

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
 # pedestrian_prediction
 Realtime, confidence-varying trajectory prediction for "Probabilistically Safe Robot Planning with Confidence-Based Human Predictions". RSS '18
 
-## Dependancies
-This repository depends on [sklearn](http://scikit-learn.org/stable/) and [scipy](https://www.scipy.org/) and on [Enum34](https://pypi.org/project/enum34/). To install both, run:
-```
-sudo pip install -U scikit-learn
-sudo apt-get install python-scipy
-sudo pip install --upgrade pip enum34
-```
-
 ## Installation
 ```
 git clone https://github.com/sirspinach/pedestrian_prediction.git
 cd pedestrian_prediction
-sudo pip install .
+pip install -e .
 ```
--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import sys
+
+if sys.version_info.major != 2:
+    print("This package is only compatible with Python 2.")
+    exit(1)
+
+extras = {
+    'plots': ['plotly']
+}
 
 setup(
     name='pedestrian_prediction',
-    version='0.0.1',
-    packages=find_packages()
-    )
-
-#setup(name='pedestrian_prediction', version='0.1.0', packages=['pedestrian_prediction'])
-
+    version='0.1',
+    long_description=open('README.md').read(),
+    long_description_context_type='text/markdown',
+    url='https://github.com/sirspinach/pedestrian_prediction',
+    author="Steven H. Wang",
+    author_email='wang.steven.h@gmail.com',
+    packages=['pp'],
+    install_requires=[
+        'enum34',
+        'scipy',
+        'scikit-learn',
+        ],
+    extras_require=extras,
+)
