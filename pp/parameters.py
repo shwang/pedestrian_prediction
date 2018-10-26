9c91f6dd46e414e472959a7bb78c540585200cc6
Steven H. Wang <wang.steven.h@gmail.com>
2018-10-25 17:05:20 -0700

    Mention value iteration in parameters.py
    I got confused about what MDP.rewards is used for.

--- pp/parameters.py
+++ pp/parameters.py
@@ -14,6 +14,9 @@ val_euclid = sys.modules['pp.mdp.euclid']
 val_hardmax = sys.modules['pp.mdp.hardmax']
 val_default = val_hardmax
 
+# Right now, only pp.mdp.classic.GridWorld uses value iteration.
+# The other mdps build q_values without explicitly using reward functions.
+
 import inference.hardmax
 inf_hardmax = sys.modules['pp.inference.hardmax']
 inf_default = inf_hardmax
